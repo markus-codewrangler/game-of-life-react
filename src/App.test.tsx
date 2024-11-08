158423044231a35a2e36ddc4ff3b6036db4efd5b
Markus Hartig <markus.hartig@justrelate.com>
2024-11-08 20:17:23 +0100

    Extend basic tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
+import { createRoot } from 'react-dom/client';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  createRoot(div).render(<App />);
+});
+
+it('renders welcome message', () => {
+  render(<App />);
+  expect(screen.getByText('Learn React')).toBeInTheDocument();
+});
 
 test('renders learn react link', () => {
   render(<App />);
